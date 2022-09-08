dd52cb7eede8a3e2b7927bd17befe6c1262b0cea
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-09-08 13:24:29 -0500

    Started In-Depth JS videos
    Made index.htm, and other JS files
    Worked on variables, assignments, namings, and constants.

---
+++ In-Depth-Javascript/namings.js
@@ -0,0 +1,24 @@
+//Variable naming
+
+//Legal
+let TEST;
+let test;
+let woman3;
+let blackDog;
+
+//Illegal
+// let 1girl; can't start variables with numbers
+// let !girl; same with special characters
+// let girl@$%$%#$$;
+// let; needs a name after keyword
+// let function;
+
+//reserved JS keywords
+//Let, const, var, function, while, for/of
+
+//Readability
+
+let somePerson = {
+    name: "James",
+    age: 10
+}
---
+++ In-Depth-Javascript/variables.js
@@ -0,0 +1,26 @@
+//var, let, const
+var x; //Before ES6, old way of declaring variables
+
+//Modern way, ES6
+let y;
+
+//const has to be initialized
+const z = 10;
+// z = 2; cannont reassign values to constants
+
+console.log("x: ", x);
+console.log("y: ", y);
+console.log("z: ", z);
+
+x = 1;
+y = 2; 
+
+console.log("x: ", x);
+console.log("y: ", y);
+console.log("z: ", z);
+
+var x;
+// let y; can't redeclare when variable has already been declared
+
+
+// let someDog;
