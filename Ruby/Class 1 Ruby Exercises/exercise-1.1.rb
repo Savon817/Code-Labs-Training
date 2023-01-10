90eed9bae94641aa6463ef7c1f80c5a6bd57153f
Savon Williams <44207109+Savon817@users.noreply.github.com>
2023-01-09 19:46:07 -0600

    Update: Fixed forgotten exercise for Exercise 1.1

--- Ruby/Class 1 Ruby Exercises/exercise-1.1.rb
+++ Ruby/Class 1 Ruby Exercises/exercise-1.1.rb
@@ -1,3 +1,8 @@
 p "p"
 puts "puts"
-print "My Name is John."
+print "My Name is John.\n"
+
+my_name = "Savon"
+beginning_sentence = "My name is "
+full_sentence = beginning_sentence + my_name
+puts full_sentence
