2cb70e3c912e45be0506cd525512e37066016ae4
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-01-09 20:44:10 -0600

    Update: Finished Exercise 1.4

--- Ruby/Class 1 Ruby Exercises/exercise-1.4.rb
+++ Ruby/Class 1 Ruby Exercises/exercise-1.4.rb
@@ -6,6 +6,8 @@ user_input_two = gets.chomp
 
 if user_input.downcase == "john" && user_input_two.downcase == "doe"
     puts "I found you!"
+elsif user_input.downcase == "amy" && user_input_two.downcase == "jeans"
+    puts "Amy! Help me look for John Doe."
 else
     puts "You're not who I'm looking for"
 end
