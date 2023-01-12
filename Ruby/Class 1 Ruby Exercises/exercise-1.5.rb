bb6910c4a79f21155819c1cead085c85ec9a17f6
Savon Williams <44207109+Savon817@users.noreply.github.com>
2023-01-12 17:53:53 -0600

    Update: Fixed argument for If-Else statement

--- Ruby/Class 1 Ruby Exercises/exercise-1.5.rb
+++ Ruby/Class 1 Ruby Exercises/exercise-1.5.rb
@@ -15,10 +15,10 @@ users = [
 
 i = 0
 while i < users.length
-    # if user[i][:name].starts_with? "Jimmy"
-    #     puts "My name and age is confidential."
-    # else
-    #     puts "My name is #{users[i][:name]} and I am #{users[i][:age]}"
-    # end
-    # i += 1
+    if users[i][:name].start_with? "Jimmy"
+        puts "My name and age is confidential."
+    else
+        puts "My name is #{users[i][:name]} and I am #{users[i][:age]}"
+    end
+    i += 1
 end 
