7bfc0b70856d76790e8988df47fab08790cfd7e6
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-01-12 17:00:54 -0600

    Add: Started Exercise 1.5

---
+++ Ruby/Class 1 Ruby Exercises/exercise-1.5.rb
@@ -0,0 +1,24 @@
+users = [
+    {
+        name: "John Doe",
+        age: 43
+    },
+    {
+        name: "Amy Singer",
+        age: 53
+    },
+    {
+        name: "Jimmy Lendricks",
+        age: 23
+    }
+]
+
+i = 0
+while i < users.length
+    # if user[i][:name].starts_with? "Jimmy"
+    #     puts "My name and age is confidential."
+    # else
+    #     puts "My name is #{users[i][:name]} and I am #{users[i][:age]}"
+    # end
+    # i += 1
+end 
