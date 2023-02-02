46bfc54f901b4173b1a7f744673a7fe658b48aca
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-02-02 16:02:12 -0600

    Update: Fake data examples

--- Ruby/code-labs-be/rails_seed_and_faker_gem_example/db/seeds.rb
+++ Ruby/code-labs-be/rails_seed_and_faker_gem_example/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(0..50).each do
+    Book.create(title: Faker::Book.title, summary: Faker::Lorem.paragraph)
+end
