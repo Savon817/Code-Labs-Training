d0a9d2ea6eda03a8af1adb855bfeac5ec8ac8661
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-02-06 21:19:40 -0600

    Update: Made Edit, Update, and Destroy CRUD actions

--- Ruby/code-labs-be/exercises/app/controllers/posts_controller.rb
+++ Ruby/code-labs-be/exercises/app/controllers/posts_controller.rb
@@ -21,4 +21,26 @@ class PostsController < ApplicationController
     def show
         @post = Post.find(params[:id])
     end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+
+        if @post.update(title: params[:post][:title], body: params[:post][:body])
+            flash[:message] = "Blog was updated successfully! :D"
+            redirect_to posts_path
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+
+        @post.destroy
+        redirect_to posts_path
+    end
 end
