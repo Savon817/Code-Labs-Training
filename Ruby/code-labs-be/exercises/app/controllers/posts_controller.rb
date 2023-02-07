04524ca733d739c3d4c97980c17a81e15bce3dc3
Savon Williams <44207109+Savon817@users.noreply.github.com>
2023-02-06 20:23:53 -0600

    Update: Create new and show templates

--- Ruby/code-labs-be/exercises/app/controllers/posts_controller.rb
+++ Ruby/code-labs-be/exercises/app/controllers/posts_controller.rb
@@ -4,6 +4,21 @@ class PostsController < ApplicationController
     end
 
     def new
-        @posts = Post.new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(title: params[:post][:title], body: params[:post][:body])
+
+        if @post.save
+            flash[:message] = "Blog was successfully saved"
+            redirect_to posts_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def show
+        @post = Post.find(params[:id])
     end
 end
