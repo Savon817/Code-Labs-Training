95fd176ffb50f88490e9a5bcb2c1e2fba3da1a8f
Savon817 <44207109+Savon817@users.noreply.github.com>
2023-02-21 17:00:26 -0600

    Update: Added Username field to view

--- Ruby/code-labs-be/Quiz-Project/app/models/user.rb
+++ Ruby/code-labs-be/Quiz-Project/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :email, :password_digest, presence: true
+    validates :username, :email, :password_digest, presence: true
     has_secure_password
     has_many :quizzes
 
