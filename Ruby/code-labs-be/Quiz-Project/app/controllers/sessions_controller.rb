f967967367cf3944564bc97350df23979b5063c2
Savon Williams <savon817@live.missouristate.edu>
2023-02-23 20:11:40 -0600

    Update: Made Flash Messages for Logging in, minor change in sign up view

--- Ruby/code-labs-be/Quiz-Project/app/controllers/sessions_controller.rb
+++ Ruby/code-labs-be/Quiz-Project/app/controllers/sessions_controller.rb
@@ -3,10 +3,19 @@ class SessionsController < ApplicationController
     end
 
     def create
-        @user = Users.find_by(email: params[:email])
+        @user = Users.find_by(username: params[:username])
 
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
+            flash[:notice] = "Successfully Logged in!"
+            # redirect_to root_path, notice: "Logged in"
+        else
+            flash[:notice] = "Invalid email or password"
+            render :new
         end
+    end
+    
+    def show
+
     end
 end
