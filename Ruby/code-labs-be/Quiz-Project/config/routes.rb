22877a7285a8ca7703c46a476d1be7e4add4cf80
Savon Williams <savon817@live.missouristate.edu>
2023-03-02 20:21:22 -0600

    Update: Made quiz routing

--- Ruby/code-labs-be/Quiz-Project/config/routes.rb
+++ Ruby/code-labs-be/Quiz-Project/config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :quizzes do
+    resources :questions, except: [:index]
+  end
 end
